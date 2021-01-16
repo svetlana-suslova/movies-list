ff4a5604e1fee64a51af189d93e94a371f6dc781
bonial_virto <svetlana.suslova2031@gmail.com>
2021-01-16 20:14:54 +0100

    added react-hook form library to the project, implemented basic  fields validation

--- src/components/MovieModal.js
+++ src/components/MovieModal.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { MyButton, CloseBtn } from './common/Buttons';
 import {TextInput, TextAreaInput, NumberInput, SelectInput} from './common/Inputs';
+import { useForm } from "react-hook-form";
+import styled from 'styled-components';
 
-const MovieModal = ({cancelEditMovie, onChangeMovie, movie, saveMovie, genres, modal}) => {
+const ErrorMessage = styled.p`
+    color: #bf1650;
+    ::before {
+        display: inline;
+        content: "⚠ ";
+    }
+`;
+
+const MovieModal = ({cancelEditMovie, onChangeMovie, movie, saveMovie, genres, modal }) => {
 
     const genresOptions = genres.filter(g => g !== "ALL").map(g => ({value: g, label: g}));
- 
+
     const selectedGenres = genresOptions.filter(go => {
         if (!movie) return false;
         return movie.genres.indexOf(go.value) >= 0;
     });
 
+    const { register, handleSubmit, errors, control } = useForm();
+    const onSubmit = () => saveMovie();
+
     const closeBtn = <CloseBtn onClickMethod={cancelEditMovie}/>
+    const errorMessage = 'This field is required.';
 
     return (
         <div>
@@ -23,59 +37,76 @@ const MovieModal = ({cancelEditMovie, onChangeMovie, movie, saveMovie, genres, m
                         Edit movie
                     </ModalHeader>
                     <ModalBody>
-                        <TextInput
-                        name="title"
-                        label="Title"
-                        placeholder="Title"
-                        onChangeMovie={onChangeMovie}
-                        value={movie.title}/>
-                        <NumberInput
-                        name="year"
-                        label="Year"
-                        placeholder="2020"
-                        onChangeMovie={onChangeMovie}
-                        min="1950"
-                        value={movie.year}/>
-                        <NumberInput
-                        name="runtime"
-                        label="Runtime"
-                        placeholder="120"
-                        onChangeMovie={onChangeMovie}
-                        min="1"
-                        value={movie.runtime}/>
-                        <SelectInput
-                        name="genres"
-                        label="Genres"
-                        options={genresOptions}
-                        onChangeMovie={onChangeMovie}
-                        value={selectedGenres}/>
-                        <TextInput
-                        name="director"
-                        label="Director"
-                        placeholder="Director"
-                        onChangeMovie={onChangeMovie}
-                        value={movie.director}/>
-                        <TextAreaInput
-                        name="actors"
-                        rows={2}
-                        label="Actors comma separated"
-                        onChangeMovie={onChangeMovie}
-                        placeholder="Actors"
-                        value={movie.actors}/>
-                        <TextAreaInput
-                        name="plot"
-                        rows={4}
-                        label="Plot"
-                        onChangeMovie={onChangeMovie}
-                        placeholder="Plot"
-                        value={movie.plot}/>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <TextInput
+                            name="title"
+                            label="Title"
+                            placeholder="Title"
+                            register={register}
+                            required
+                            onChangeMovie={onChangeMovie}
+                            defaultValue={movie.title} />
+                            {errors.title && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                            <SelectInput
+                            name="genres"
+                            label="Genres"
+                            options={genresOptions}
+                            onChangeMovie={onChangeMovie}
+                            defaultValue={selectedGenres}
+                            control={control} />
+                            {errors.genres && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                            <NumberInput
+                            name="year"
+                            label="Year"
+                            placeholder="2020"
+                            onChangeMovie={onChangeMovie}
+                            min="1950"
+                            defaultValue={movie.year}
+                            register={register}
+                            required />
+                            {errors.year && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                            <NumberInput
+                            name="runtime"
+                            label="Runtime"
+                            placeholder="120"
+                            onChangeMovie={onChangeMovie}
+                            min="1"
+                            defaultValue={movie.runtime}
+                            register={register}
+                            required />
+                            {errors.runtime && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                            <TextInput
+                            name="director"
+                            label="Director"
+                            placeholder="Director"
+                            onChangeMovie={onChangeMovie}
+                            defaultValue={movie.director}
+                            register={register}
+                            required />
+                            {errors.director && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                            <TextAreaInput
+                            name="actors"
+                            rows="2"
+                            label="Actors comma separated"
+                            onChangeMovie={onChangeMovie}
+                            placeholder="Actors"
+                            defaultValue={movie.actors}
+                            register={register}
+                            required />
+                            {errors.actors && <ErrorMessage>{errorMessage}</ErrorMessage>}
+                            <TextAreaInput
+                            name="plot"
+                            rows="4"
+                            label="Plot"
+                            onChangeMovie={onChangeMovie}
+                            placeholder="Plot"
+                            defaultValue={movie.plot}
+                            register={register} />
+                            <button type="submit">Save</button>
+                            <MyButton size="sm" color="secondary" 
+                            title="Cancel"onClickMethod={cancelEditMovie}/>
+                        </form>
                     </ModalBody>
-                    <ModalFooter>
-                        <MyButton size="sm" color="primary" 
-                        title="Save" onClickMethod={saveMovie}/>
-                        <MyButton size="sm" color="secondary" 
-                        title="Cancel"onClickMethod={cancelEditMovie}/>
-                    </ModalFooter>
                 </Modal>
                 : null
             }
--- src/components/common/Inputs.js
+++ src/components/common/Inputs.js
@@ -1,87 +1,119 @@
 import React from 'react';
-import { FormGroup, Label, Input, FormFeedback } from 'reactstrap';
+import { FormGroup } from 'reactstrap';
 import styled from 'styled-components';
 import Select from 'react-select';
 
-const LabelBold = styled(Label)`
+const LabelBold = styled.label`
     font-weight: 700;
 `;
-const MyInput = ({name, label, placeholder, rows, min, type, ...props}) => {
+
+const Input = styled.input`
+    display: block;
+    width: 100%;
+    height: calc(1.5em + 0.75rem + 2px);
+    border-radius: 4px;
+    border: 1px solid #ced4da;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    padding: 0.375rem 0.75rem;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    margin-bottom: 15px;
+`;
+
+const TextArea = styled.textarea`
+    display: block;
+    width: 100%;
+    border-radius: 4px;
+    border: 1px solid #ced4da;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    padding: 0.375rem 0.75rem;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    margin-bottom: 15px;
+`;
+
+
+export const TextInput = ({name, placeholder, label, onChangeMovie, defaultValue, register, required }) => {
+
+    const inputOnChange = (event) => {
+        onChangeMovie(event.target.name, event.target.value);
+    };
     return (
         <FormGroup>
-            <LabelBold htmlFor={name}>{label}</LabelBold>
-            <Input type={type} 
-            name={name} 
+            <LabelBold>{label}</LabelBold>
+            <Input
+            type="text"
+            name={name}
             placeholder={placeholder}
-            rows={rows}
-            min={min}
-            {...props}/>
-            <FormFeedback>Field is required.</FormFeedback>
+            onChange={inputOnChange}
+            defaultValue={defaultValue}
+            ref={register({ required })}/>
         </FormGroup>
     );
 }
 
-
-export const TextInput = ({onChangeMovie, value, ...props}) => {
-
-    const inputOnChange = (event) => {
-        onChangeMovie(event.target.name, event.target.value);
-    };
-    return (
-        <MyInput
-        type="text"
-        onChange={inputOnChange}
-        value={value ? value : ''}
-        {...props}/>
-    );
-}
-export const TextAreaInput = ({onChangeMovie, rows, value, ...props}) => {
+export const TextAreaInput = ({name, placeholder, label, onChangeMovie, defaultValue, register, rows, required}) => {
 
     const inputOnChange = (event) => {
         onChangeMovie(event.target.name, event.target.value);
     };
     return (
-        <MyInput
-        type="textarea"
-        rows={rows}
-        onChange={inputOnChange}
-        value={value ? value : ''}
-        {...props}/>
+        <FormGroup>
+            <LabelBold>{label}</LabelBold>
+            <TextArea
+            name={name}
+            rows={rows}
+            placeholder={placeholder}
+            onChange={inputOnChange}
+            defaultValue={defaultValue}
+            ref={register({ required })}/>
+        </FormGroup>
+
     );
 }
-export const NumberInput = ({onChangeMovie, min, value, ...props}) => {
+
+export const NumberInput = ({name, placeholder, label, onChangeMovie, min, defaultValue, register, required}) => {
 
     const inputOnChange = (event) => {
         const value = parseInt(event.target.value, 10);
         onChangeMovie(event.target.name, value);
     };
     return (
-        <MyInput
-        type="number"
-        onChange={inputOnChange}
-        min={min}
-        value={value}
-        {...props}/>
+        <FormGroup>
+            <LabelBold>{label}</LabelBold>
+            <Input
+            type="number"
+            name={name}
+            placeholder={placeholder}
+            min={min}
+            onChange={inputOnChange}
+            defaultValue={defaultValue}
+            ref={register({ required })}/>
+        </FormGroup>
     );
 }
 
-export const SelectInput = ({name, label, placeholder, onChangeMovie, options, value}) => {
+export const SelectInput = ({name, label, placeholder, onChangeMovie, options, defaultValue, register, required}) => {
 
     const inputOnChange = (val) => {
         if (!val) return;
         const value = val.map(x => x.value);
         onChangeMovie(name, value);
     };
+
     return (
         <FormGroup>
-            <LabelBold htmlFor={name}>{label}</LabelBold>
-            <Select isMulti 
+            <LabelBold>{label}</LabelBold>
+            <Select isMulti
             name={name} 
             placeholder={placeholder}
             onChange={inputOnChange}
             options={options}
-            value={value}/>
-            <FormFeedback>Field is required.</FormFeedback>
+            defaultValue={defaultValue}/>
         </FormGroup>
     );
 }
