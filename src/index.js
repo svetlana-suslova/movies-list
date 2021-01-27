398fc78455308463463e724bea627695bc73fac3
bonial_virto <svetlana.suslova2031@gmail.com>
2021-01-27 21:53:46 +0100

    small fix of import in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../node_modules/toastr/build/toastr.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'toastr/build/toastr.css';
 import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+     <React.StrictMode>
+          <App />
+     </React.StrictMode>,
+     document.getElementById('root')
 );
